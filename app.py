f0363e44da21ee9d10338d1e1e94b3c6aafd9a1b
Joyce <joyyh20@gmail.com>
2024-11-05 08:44:32 -0300

    dtrt

--- app.py
+++ app.py
@@ -4,19 +4,31 @@ from flask import Flask, render_template
 # Cria um aplicativo Flask chamado "app"
 app = Flask(__name__)
 
-# Rota raiz, equivalente a páginai inicial do site (index)
-@app.route("/")
-def home(): # Função executada ao acessar a rota raiz
+# Cria um usuário "fake" para testes
+# No futuro, isso virá de um cookie
+usuario = {
+    'nome': 'Joca da Silva',
+    'id': '1'
+}
+# Extrai apenas o primeiro nome do usuário
+usuario['pnome'] = usuario['nome'].split()[0]
 
-    # Cria um usuário "fake" para testes
-    # No futuro, isso virá de um cookie
-    usuario = {
-        'nome': 'Joca da Silva',
-        'id': '1'
+
+@app.route("/")  # Rota raiz, equivalente a página inicial do site (index)
+def index():  # Função executada ao acessar a rota raiz
+
+    # Dados, variáveis e valores a serem passados para o template HTML
+    pagina = {
+        'titulo': 'CRUDTrecos',
+        'usuario': usuario
     }
-    
-    # Extrai apenas o primeiro nome do usuário
-    usuario['pnome'] = usuario['nome'].split()[0]
+
+    # Renderiza o template HTML, passando valores (pagina) para ele
+    return render_template('index.html', **pagina)
+
+
+@app.route('/novo')  # Rota para a página de cadastro de novo treco
+def novo():  # Função executada para cadastrar novo treco
 
     # Dados, variáveis e valores a serem passados para o template HTML
     pagina = {
@@ -25,32 +37,7 @@ def home(): # Função executada ao acessar a rota raiz
     }
 
     # Renderiza o template HTML, passaod valores para ele
-    return render_template('home.html', **pagina)
-
-
-
-@app.route('/novo')
-def novo():
-
-    # Cria um usuário "fake" para testes
-    # No futuro, isso virá de um cookie
-    usuario = {
-        'nome': 'Joca da Silva',
-        'id': '1'
-    }
-    
-    # Extrai apenas o primeiro nome do usuário
-    usuario['pnome'] = usuario['nome'].split()[0]
-
-    # Dados, variáveis e valores a serem passados para o template HTML
-    pagina = {
-        'titulo': 'CRUDTrecos',
-        'usuario': usuario
-    }
-
-    # Renderiza o template HTML, passaod valores para ele
-    return render_template('home.html', **pagina)
-
+    return render_template('index.html', **pagina)
 
 
 # Executa o servidor HTTP se estiver no modo de desenvolvimento
